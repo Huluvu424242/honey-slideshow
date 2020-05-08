701404b711946896cb1bd926496428fc633f13de
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-05-08 23:50:47 +0200

    [feature/setup] svg optimiert

--- src/components/honey-slideshow/constants.ts
+++ src/components/honey-slideshow/constants.ts
@@ -1,103 +1,135 @@
 export const IMG_START: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m325.51443 307.29922l-277.20996 -141.39633l279.07086 -139.53543l-1.8609009 40.931755l-191.62729 98.60368l193.48819 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m325.51443 307.29922l-277.20996 -141.39633l279.07086 -139.53543l-1.8609009 40.931755l-191.62729 98.60368l193.48819 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path fill="#000000" d="m48.31496 329.7953l-48.31496 0l0 -329.7953l48.31496 0z" fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m48.31496 329.7953l-48.31496 0l0 -329.7953l48.31496 0z" fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m954.5431 670.8872l-812.8905 -308.69644l818.3474 -304.63376l-5.456909 89.36221l-561.92786 215.27156l567.38477 219.33426z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m954.5431 670.8872l-812.8905 -308.69644l818.3474 -304.63376l-5.456909 89.36221l-561.92786 215.27156l567.38477 219.33426z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000" d="m141.67947 720.0l-141.6693 0l0 -720.0l141.6693 0z" fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m141.67947 720.0l-141.6693 0l0 -720.0l141.6693 0z" fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_FASTREWIND: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m260.01376 303.42783l-156.01376 -141.39633l157.0611 -139.53543l-1.0473328 40.93176l-107.84784 98.60367l108.89517 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m260.01376 303.42783l-156.01376 -141.39633l157.0611 -139.53543l-1.0473328 40.93176l-107.84784 98.60367l108.89517 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m156.01378 303.42783l-156.01378 -141.39633l157.06108 -139.53543l-1.0473022 40.93176l-107.84785 98.60367l108.89515 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m156.01378 303.42783l-156.01378 -141.39633l157.06108 -139.53543l-1.0473022 40.93176l-107.84785 98.60367l108.89515 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m956.60803 718.49603l-573.71375 -362.3846l577.56506 -357.61536l-3.8512573 104.90401l-396.59186 252.71136l400.4431 257.48062z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m956.60803 718.49603l-573.71375 -362.3846l577.56506 -357.61536l-3.8512573 104.90401l-396.59186 252.71136l400.4431 257.48062z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m574.16724 718.49603l-573.71375 -362.3846l577.56506 -357.61536l-3.8513184 104.90401l-396.59186 252.71136l400.44318 257.48062z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m574.16724 718.49603l-573.71375 -362.3846l577.56506 -357.61536l-3.8513184 104.90401l-396.59186 252.71136l400.44318 257.48062z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_REWIND: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m277.20996 303.42783l-277.20996 -141.39633l279.07086 -139.53543l-1.8609009 40.93176l-191.62729 98.60367l193.48819 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m277.20996 303.42783l-277.20996 -141.39633l279.07086 -139.53543l-1.8609009 40.93176l-191.62729 98.60367l193.48819 100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m951.5988 722.2808l-951.6064 -364.2935l957.99445 -359.49908l-6.3880615 105.45658l-657.81824 254.04251l664.2063 258.83694z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m951.5988 722.2808l-951.6064 -364.2935l957.99445 -359.49908l-6.3880615 105.45658l-657.81824 254.04251l664.2063 258.83694z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_PLAY: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '  <g>\n' +
-  '    <path fill="#000000" d="m0 0l276.6299 141.48032l-276.6299 141.48032z" fill-rule="evenodd"/>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000" d="m-0.007874016 0l958.3937 360.86615l-958.3937 360.86615z" fill-rule="evenodd"/>\n' +
   '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '          d="m0 0l276.6299 141.48032l-276.6299 141.48032z" fill-rule="evenodd"/>\n' +
+  '          d="m-0.007874016 0l958.3937 360.86615l-958.3937 360.86615z" fill-rule="evenodd"/>\n' +
   '  </g>\n' +
-  '</svg>\n';
+  '</svg>';
 export const IMG_PAUSE: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000" d="m35.89241 1.2073491l72.15748 0l0 289.92126l-72.15748 0z" fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m35.89241 1.2073491l72.15748 0l0 289.92126l-72.15748 0z" fill-rule="evenodd"/>\n' +
-  '        <path fill="#000000" d="m167.98764 1.2073491l72.157486 0l0 289.92126l-72.157486 0z" fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m167.98764 1.2073491l72.157486 0l0 289.92126l-72.157486 0z" fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000" d="m198.31253 1.2073491l192.50394 0l0 720.0l-192.50394 0z" fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m198.31253 1.2073491l192.50394 0l0 720.0l-192.50394 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000" d="m550.7434 1.2073491l192.50397 0l0 720.0l-192.50397 0z" fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m550.7434 1.2073491l192.50397 0l0 720.0l-192.50397 0z" fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_FOREWARD: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m1.8608924 22.496063l277.20996 141.39633l-279.07086 139.53543l1.8608924 -40.931763l191.6273 -98.60367l-193.48819 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m1.8608924 22.496063l277.20996 141.39633l-279.07086 139.53543l1.8608924 -40.931763l191.6273 -98.60367l-193.48819 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m6.4014697 -1.503937l953.6023 362.38464l-960.0038 357.6154l6.4014697 -104.90405l659.1979 -252.71133l-665.59937 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m6.4014697 -1.503937l953.6023 362.38464l-960.0038 357.6154l6.4014697 -104.90405l659.1979 -252.71133l-665.59937 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_FASTFOREWARD: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m1.0473102 22.496063l156.01376 141.39633l-157.06108 139.53543l1.0473102 -40.931763l107.84784 -98.60367l-108.89515 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m1.0473102 22.496063l156.01376 141.39633l-157.06108 139.53543l1.0473102 -40.931763l107.84784 -98.60367l-108.89515 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m105.04731 22.496063l156.0138 141.39633l-157.0611 139.53543l1.0473099 -40.931763l107.84785 -98.60367l-108.89516 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m105.04731 22.496063l156.0138 141.39633l-157.0611 139.53543l1.0473099 -40.931763l107.84785 -98.60367l-108.89516 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m3.8513029 -1.503937l573.71375 362.38464l-577.56506 357.6154l3.8513029 -104.90405l396.59186 -252.71133l-400.44315 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m3.8513029 -1.503937l573.71375 362.38464l-577.56506 357.6154l3.8513029 -104.90405l396.59186 -252.71133l-400.44315 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m386.29053 -1.503937l573.71375 362.38464l-577.56506 357.6154l3.8512878 -104.90405l396.59186 -252.71133l-400.44315 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m386.29053 -1.503937l573.71375 362.38464l-577.56506 357.6154l3.8512878 -104.90405l396.59186 -252.71133l-400.44315 -257.48065z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
   '</svg>';
 export const IMG_END: string = '<svg version="1.1" viewBox="0.0 0.0 960.0 720.0" fill="none" stroke="none" stroke-linecap="square"\n' +
   '     stroke-miterlimit="10" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">\n' +
-  '    <g>\n' +
-  '        <path fill="#000000"\n' +
-  '              d="m1.8608924 22.496063l277.20996 141.39633l-279.07086 139.53543l1.8608924 -40.931763l191.6273 -98.60367l-193.48819 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m1.8608924 22.496063l277.20996 141.39633l-279.07086 139.53543l1.8608924 -40.931763l191.6273 -98.60367l-193.48819 -100.46457z"\n' +
-  '              fill-rule="evenodd"/>\n' +
-  '        <path fill="#000000" d="m279.06036 0l48.314972 0l0 329.7953l-48.314972 0z" fill-rule="evenodd"/>\n' +
-  '        <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
-  '              d="m279.06036 0l48.314972 0l0 329.7953l-48.314972 0z" fill-rule="evenodd"/>\n' +
-  '    </g>\n' +
-  '</svg>';
+  '  <clipPath id="p.0">\n' +
+  '    <path d="m0 0l960.0 0l0 720.0l-960.0 0l0 -720.0z" clip-rule="nonzero"/>\n' +
+  '  </clipPath>\n' +
+  '  <g clip-path="url(#p.0)">\n' +
+  '    <path fill="#000000" fill-opacity="0.0" d="m0 0l960.0 0l0 720.0l-960.0 0z" fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000"\n' +
+  '          d="m5.486655 49.11279l817.3259 308.69644l-822.81256 304.63376l5.486655 -89.36218l564.99396 -215.27158l-570.4806 -219.33423z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m5.486655 49.11279l817.3259 308.69644l-822.81256 304.63376l5.486655 -89.36218l564.99396 -215.27158l-570.4806 -219.33423z"\n' +
+  '          fill-rule="evenodd"/>\n' +
+  '    <path fill="#000000" d="m822.78174 0l142.45667 0l0 720.0l-142.45667 0z" fill-rule="evenodd"/>\n' +
+  '    <path stroke="#000000" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt"\n' +
+  '          d="m822.78174 0l142.45667 0l0 720.0l-142.45667 0z" fill-rule="evenodd"/>\n' +
+  '  </g>\n' +
+  '</svg>\n';

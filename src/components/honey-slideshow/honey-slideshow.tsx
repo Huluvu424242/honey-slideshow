0cdd1cba8dac9f1fd4287b6d4dfba6bc85b9b605
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-05-09 19:25:41 +0200

    [feature/setup] Styling fürs Control Panel erstellt

--- src/components/honey-slideshow/honey-slideshow.tsx
+++ src/components/honey-slideshow/honey-slideshow.tsx
@@ -19,7 +19,6 @@ import {
 export class HoneySlideshow {
 
 
-
   isPlaying() {
     return false;
   }
@@ -27,23 +26,35 @@ export class HoneySlideshow {
   render() {
     return (
       <host>
-        <div id={"slide-control"} class={"flex-container"}>
-          <div class="flex-content" innerHTML={IMG_START}/>
-          <div class="flex-content" innerHTML={IMG_FASTREWIND}/>
-          <div class="flex-content" innerHTML={IMG_REWIND}/>
+        <header>
+          <h2>Titel der Präsentation</h2>
+        </header>
+        <nav>
+          <div id={"slide-control"} class={"flex-container"}>
+            <div class="flex-content" title={"Zur ersten Folie"} innerHTML={IMG_START}/>
+            <div class="flex-content" title={"10 Folien zurück"} innerHTML={IMG_FASTREWIND}/>
+            <div class="flex-content" title={"1 Folie zurück"} innerHTML={IMG_REWIND}/>
 
-          <div class="flex-content" innerHTML=
-                  {this.isPlaying()
-                    ? IMG_PAUSE
-                    : IMG_PLAY
-                  }>
+            <div class="flex-content"
+                 title =
+                   {this.isPlaying()
+                     ? "Sprachausgabe beenden"
+                     : "Vortrag beginnen lassen"
+                   }
+                 innerHTML=
+                   {this.isPlaying()
+                     ? IMG_PAUSE
+                     : IMG_PLAY
+                   }>
+            </div>
+
+            <div class="flex-content" title={"1 Folie weiter"} innerHTML={IMG_FOREWARD}/>
+            <div class="flex-content" title={"10 Folien weiter"} innerHTML={IMG_FASTFOREWARD}/>
+            <div class="flex-content" title={"Zur letzten Folie"} innerHTML={IMG_END}/>
+            <div id="tags" title={"Schlagwörter zur Präsentation"} class="flex-content">tag1 tag2 tag3 tag4</div>
           </div>
-
-          <div class="flex-content" innerHTML={IMG_FOREWARD}/>
-          <div class="flex-content" innerHTML={IMG_FASTFOREWARD}/>
-          <div class="flex-content" innerHTML={IMG_END}/>
-        </div>
-        <div id={"slide-area"}/>
+          <div id={"slide-area"}/>
+        </nav>
       </host>
     );
   }

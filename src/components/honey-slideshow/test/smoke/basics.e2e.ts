d9f386c2985961094d7998102c26fd71ef180749
FunThomas424242 <huluvu424242@gmail.com>
2020-07-06 22:29:28 +0200

    [#41/axe-e2e] tests added
    Signed-off-by: FunThomas424242 <huluvu424242@gmail.com>

--- src/components/honey-slideshow/test/smoke/basics.e2e.ts
+++ src/components/honey-slideshow/test/smoke/basics.e2e.ts
@@ -1,17 +1,34 @@
 import {newE2EPage} from '@stencil/core/testing';
+import {E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
 
 describe('example', () => {
-  it('should render a foo-component', async () => {
-    const page = await newE2EPage();
+
+  let page: E2EPage;
+
+
+  beforeEach(async () => {
+    page = await newE2EPage();
     await page.setContent(
       `<honey-slideshow id="designrules" baseurl="https://funthomas424242.github.io/foile-pile/evaluationen/community-tools/">
                 <h2 slot="title">Evaluierung Community Tools</h2>
                 <div id="slidewin" slot="slide-area" class="slides"></div>
             </honey-slideshow>`
     );
+  });
+
+  it('should show all navigation buttons', async () => {
+
     const el = await page.find('honey-slideshow');
     expect(el).not.toBeNull();
-    const elm = await page.find('honey-slideshow >>> #playbutton');
+
+    let elm = await page.find('honey-slideshow >>> #startpos');
+    expect(elm.getAttribute('title')).toEqualText('Zur ersten Folie');
+
+    elm = await page.find('honey-slideshow >>> #endpos');
+    expect(elm.getAttribute('title')).toEqualText('Zur letzten Folie');
+
+     elm = await page.find('honey-slideshow >>> #playbutton');
     expect(elm.getAttribute('title')).toEqualText('Vortrag beginnen lassen');
+
   });
 });
